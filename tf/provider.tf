ae4ab07242335944dbd700b817f65dae56e6705e
bearthatcares <bearthatcares.eve@gmail.com>
2023-11-10 22:55:33 -0500

    add tf and basic fastapi application

---
+++ tf/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "minmatar-tfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
